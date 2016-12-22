92222d89f0fcb9971c37c1a7d890f8a20edc3ff2
Roy <rhh@crosscompute.com>
2016-12-22 11:05:04 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-types',
-    version='0.6.1',
+    version='0.6.2',
     description='Default data type plugins for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
