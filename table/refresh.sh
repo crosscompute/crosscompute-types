e3bf0a60f59f188b6535837ac825dca7e2d3f6de
Roy Hyunjin Han <rhh@crosscompute.com>
2017-01-22 22:16:52 +0000

    Use contenteditable

--- table/refresh.sh
+++ table/refresh.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-SCRIPT_NAMES="
-mindmup-editabletable.js
-crosscompute-table.js
-"
-pushd node_modules/crosscompute-table > /dev/null
-cat $SCRIPT_NAMES > index.js
-popd > /dev/null
 refresh-assets \
     invisibleroads_uploads \
     crosscompute_table
