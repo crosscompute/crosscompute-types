d96f70eb2ebac45e9aaee9311e03cce9f0200ade
Roy Hyunjin Han <rhh@crosscompute.com>
2017-02-01 12:01:28 -0500

    Fix crosscompute-text

--- text/setup.py
+++ text/setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-text',
-    version='0.6.3',
+    version='0.6.3.1',
     description='Text data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
