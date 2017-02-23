a5221ade34230cc765c81ae37ce01f13139fd50b
Roy Hyunjin Han <rhh@crosscompute.com>
2017-02-23 15:46:51 -0500

    Bump version

--- table/setup.py
+++ table/setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-table',
-    version='0.6.5',
+    version='0.6.5.1',
     description='Table data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
