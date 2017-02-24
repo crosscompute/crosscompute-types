369413c1676b31f2f63065547750f3da9eda388e
Roy Hyunjin Han <rhh@crosscompute.com>
2017-02-24 14:36:48 -0500

    Honor uploaded files

--- geotable/crosscompute_geotable/__init__.py
+++ geotable/crosscompute_geotable/__init__.py
@@ -165,8 +165,8 @@ def import_geotable(request):
 
 
 def _interpret(table):
-    if len(table) > 100:
-        table = table[:100]
+    if len(table) > 50:
+        table = table[:50]
     items, properties = [], {}
     geometry_column_names = get_geometry_column_names(table.columns)
     if not geometry_column_names:
